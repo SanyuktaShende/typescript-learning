08b7922dc04a27cc50953d68c774683237d2c56a
Sanyukta Shende <sanyuktashende7@gmail.com>
2026-03-21 15:21:44 +0530

    'practice'

---
+++ src/fucnt.ts
@@ -0,0 +1,6 @@
+function calculateTax (income: number): number{
+	if(income<50_000){
+		return income*1.2;
+		}
+	return income * 1.3;
+}
---
+++ src/objects.js
@@ -0,0 +1,7 @@
+var employee = {
+    id: 1,
+    name: 'Sanyukta',
+    retire: function (date) {
+        console.log(date);
+    }
+};
