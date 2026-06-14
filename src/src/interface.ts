2c9579b761a24d079243518374316151210984d8
Sanyukta Shende <sanyuktashende7@gmail.com>
2026-06-14 18:17:23 +0530

    new commits

--- src/src/interface.ts
+++ src/src/interface.ts
@@ -2,18 +2,18 @@
 
 // difference between object and interface is that interface is template and obj is actual values
 interface User {
-	name: string,
+	name : string,
 	email: string,
-	password: string;
+	password:string;
 }
 
 
-const obj = {		
-	name: 'San',
+const obj = {
+	name:'San',
 	email: 'san@gmail.com',
 	password: 'Billie@124'
 }
 
-function getDataOfUser(obj: User) {
-
+function getDataOfUser (obj: User){
+	
 }
